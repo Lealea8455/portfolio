c074307edcc11b14d472514eb419d03d27a35260
Lealea8455 <lealea8455@gmail.com>
2020-07-27 16:29:56 +0300

    Set up basic structure to main components

---
+++ client/src/components/About.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+
+function About() {
+  return (
+    <Fragment>
+      <h1>About me</h1>
+      <p>My name is Lea Gront. I'm a junior Full-stack developer. </p>
+      <p>I currently live in Petah Tikva, with my fiance and our little boy( = big spoiled doggo) Dambo.</p>
+
+      <p>LOVE plants, outdoor activities, cooking (sometimes it fails), DIY projects and coffee.</p>
+    </Fragment>
+  );
+}
+
+export default About;
