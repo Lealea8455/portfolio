4cb6be1a6889faa9ad80224a65011a36c67f7af7
Lealea8455 <lealea8455@gmail.com>
2020-08-02 18:07:21 +0300

    Minor text changings

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,17 +8,18 @@ function Landing() {
       <p>I am a junior Full-stack developer. Graduated with a bachelor degree in computer science. <br/>
         Have 1 year of hands-on experience as a front-end developer, and now I'm looking for my next big challenge.</p> 
       <p>Well-organized person, problem solver with high attention to details. Friendly and have great social skills. <br/>
-         I love web development, and fascinated by it. On my free time, I develope different web projects, learn new technologies, complete many online courses and always looking for something interesting and geeky to do. 
+         I love web development. On my free time, I develope different web projects, take online courses and always looking for something interesting. 
+         Not afraid to explore and learn new technologies!
       </p>
 
       <div className="social-icons">
-        <a href="https://www.facebook.com/profile.php?id=100003286624486" target="_blank">
+        <a href="https://www.facebook.com/profile.php?id=100003286624486" target="_blank" rel="noopener noreferrer">
           <span><FontAwesomeIcon icon={['fab', 'facebook-f']} /></span>
         </a>
-        <a href="https://www.linkedin.com/in/lea-gront/" target="_blank">
+        <a href="https://www.linkedin.com/in/lea-gront/" target="_blank" rel="noopener noreferrer">
           <span><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></span>
         </a>
-        <a href="https://github.com/Lealea8455" target="_blank">
+        <a href="https://github.com/Lealea8455" target="_blank" rel="noopener noreferrer">
           <span><FontAwesomeIcon icon={['fab', 'github']} /></span>
         </a>
       </div>
